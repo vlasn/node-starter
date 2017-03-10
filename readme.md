96550a06871b2db841b257f2e756af2c79e27ee4
vlasn <vlasn@tlu.ee>
2017-03-10 13:00:38 +0200

    Lisasin näite ja nodemoni soovituse

--- readme.md
+++ readme.md
@@ -1,5 +1,11 @@
 1) git clone
 2) installi nodejs ja npm
+
 käsurealt:
+
 3) npm install
 3) node server
+
+Hea mõte on installida NodeMon : npm install nodemon.
+
+Peale seda saab autorestartiva node serveri käivitada: nodemon server.js
--- server.js
+++ server.js
@@ -4,12 +4,12 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-    var massiiv = {
-        yks: 1,
-        kaks: 2
-    };
-    res.json(massiiv);
+app.get('/korruta/', function (req, res) {
+    var arv1 = req.query.arv1;
+    var arv2 = req.query.arv2;
+    var tulem = arv1 * arv2;
+    res.json(tulem);
+    //Testimiseks url: localhost:3000/korruta?arv1=7&arv2=16
 });
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
