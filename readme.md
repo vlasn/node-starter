507febb82807a5f96b47d63360c5647776949865
vlasn <vlasn@tlu.ee>
2017-03-10 10:08:03 +0200

    added readme

---
+++ readme.md
@@ -0,0 +1,5 @@
+1) git clone
+2) installi nodejs ja npm
+käsurealt:
+3) npm install
+3) node server
--- server.js
+++ server.js
@@ -5,7 +5,11 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.json("sup");
+    var massiiv = {
+        yks: 1,
+        kaks: 2
+    };
+    res.json(massiiv);
 });
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
